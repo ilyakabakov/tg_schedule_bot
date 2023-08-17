be330f38aafff382b579f58af516941b2451e433
ilyakabakov <108796040+ilyakabakov@users.noreply.github.com>
2023-08-17 12:50:41 +0400

    It is my PET Project Telegram bot to apply for a consultation In this version: - Memory storage changed on Redis. - Changed the interaction with database, now all requests are async, using SQLAlchemy with aiosqlite. - Removed file with all previous SQL requests. - Most handlers methods now using edit_text(it's faster than delete and send new message...) - Redesigned main json archieve structure with content. - Now need to request 1 async func for interaction with json data file. - Added and edited annotations for functions, handlers, coroutines.

--- database/json_queries.py
+++ database/json_queries.py
@@ -9,14 +9,12 @@ with open(f_name, 'r', encoding="utf-8") as file:
     data_json = json.load(file)
 
 """ Coroutines for response from json data """
+
+
 async def array_json(user: str, query: str):
     try:
-        array = []
         data = await get_data_json(user)
-
-        for text in data[query]:
-            array.append(text)
-        return array[0]
+        return data[query]
     except Exception as ex:
         print(f'Func array_json failed ERROR: {ex}')
 
